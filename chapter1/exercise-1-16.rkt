971270085bcaab35594765b8c0858adb3bce6fb8
Sven Panne <svenpanne@gmail.com>
2019-06-23 20:31:02 +0200

    Added alternative implementation via map.

--- chapter1/exercise-1-16.rkt
+++ chapter1/exercise-1-16.rkt
@@ -2,9 +2,16 @@
 ; ------------------------------------------------------------------------------
 ; Exercise 1.16
 
+; Alternative 1: Recurse via outer list structure.
 (define invert
   (lambda (lst)
     (if (null? lst)
         '()
         (cons (list (cadr (car lst)) (car (car lst)))
               (invert (cdr lst))))))
+
+; Alternative 2: Recursion hidden in map.
+(define invert-via-map
+  (lambda (lst)
+    (map (lambda (lst2) (list (cadr lst2) (car lst2)))
+         lst)))
