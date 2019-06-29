985cbe39da54cae686de2504124ef05bbb4030cd
Sven Panne <svenpanne@gmail.com>
2019-06-29 17:14:44 +0200

    Tiny cleanups. Added factorial function.

--- chapter2/exercise-2-01.rkt
+++ chapter2/exercise-2-01.rkt
@@ -15,12 +15,32 @@
 (define successor-bignum
   (lambda (n)
     (cond ((null? n) '(1))
-          ((= (+ (car n) 1) N) (cons 0 (successor-bignum (cdr n))))
+          ((= (car n) (- N 1)) (cons 0 (successor-bignum (cdr n))))
           (else (cons (+ (car n) 1) (cdr n))))))
 
 (define predecessor-bignum
   (lambda (n)
     (cond ((null? n) (eopl:error 'predecessor-bignum "zero.~%"))
+          ((equal? n '(1)) '())
           ((zero? (car n)) (cons (- N 1) (predecessor-bignum (cdr n))))
-          ((and (= (car n) 1) (null? (cdr n))) '())
           (else (cons (- (car n) 1) (cdr n))))))
+
+; for measurements
+
+(define plus
+  (lambda (x y)
+    (if (is-zero?-bignum x)
+        y
+        (successor-bignum (plus (predecessor-bignum x) y)))))
+
+(define multiply
+  (lambda (x y)
+    (if (is-zero?-bignum x)
+        (zero-bignum)
+        (plus (multiply (predecessor-bignum x) y) y))))
+
+(define factorial
+  (lambda (n)
+    (if (is-zero?-bignum n)
+        (successor-bignum (zero-bignum))
+        (multiply n (factorial (predecessor-bignum n))))))
