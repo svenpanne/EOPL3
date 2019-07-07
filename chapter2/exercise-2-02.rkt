3fc14c4dbb1c53a37bd67399eba34cea8f8234cc
Sven Panne <svenpanne@gmail.com>
2019-07-07 19:18:00 +0200

    Fixed typos.

--- chapter2/exercise-2-02.rkt
+++ chapter2/exercise-2-02.rkt
@@ -2,8 +2,8 @@
 ; ------------------------------------------------------------------------------
 ; Exercise 2.2
 ;
-; The unary representation is quite efficient, but fulfills the specification.
-; The representation using Scheme number is only correct iff the underlying
+; The unary representation is quite inefficient, but fulfills the specification.
+; The representation using Scheme numbers is only correct iff the underlying
 ; Scheme numbers support arbitrary precision integers. The bignum representation
 ; is relatively efficient, especially for larger N, and fulfills the
 ; specification.
